ee8865a50621060e215e850f9c034ba0ad966c04
Paavo Palomäki <paavo.palomaki@gmail.com>
2024-12-18 20:57:56 +0200

    Add comments to settings file

--- main/include/settings.h
+++ main/include/settings.h
@@ -11,21 +11,22 @@
 #define SSD1306_SCL_PIN 23
 #define SSD1306_RST_PIN -1
 // OUTPUT
-#define OUTPUT_PIN 21
-#define LED_PIN 2
+#define OUTPUT_PIN 2
+#define LED_PIN 15
 
 // ******* OTHER SETTINGS *******
 // OUTPUT
 #define OUTPUT_ACTIVATION_DURATION 100 // milliseconds
-#define BPM_START 80
-#define SIGNATURE_START 0
+#define BPM_START 80                   // BPM to start with
+#define SIGNATURE_START 0              // index
+
 // INPUT
 #define FAST_CHANGE_MULTIPLIER 5
-#define DOUBLE_CLICK_US 5E5
-#define FAST_CHANGE_US 1E5
-#define FAST_CHANGE_EXPIRE_US 1E6
-#define ENC_A_DEBOUNCE 1000
-#define ENC_B_DEBOUNCE 1000
-#define ENC_SW_DEBOUNCE 1000
+#define DOUBLE_CLICK_US 5E5       // microseconds
+#define FAST_CHANGE_US 1E5        // microseconds
+#define FAST_CHANGE_EXPIRE_US 1E6 // microseconds
+#define ENC_A_DEBOUNCE 1000       // microseconds
+#define ENC_B_DEBOUNCE 1000       // microseconds
+#define ENC_SW_DEBOUNCE 100000    // microseconds
 
 #endif // SETTINGS_H
